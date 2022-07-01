efad20705d3c2e3054657a6fcd681406b9fa7666
Imani-ES <imanimuh@buffalo.edu>
2022-07-01 10:50:43 -0400

    install front end packages

---
+++ blockchain/tutorial_new/client/index.js
@@ -0,0 +1,5 @@
+import Web3 from 'web3';
+import 'bootstrap/dist/css/bootstrap.css'
+import config from '../build/contracts/Tickets.json'
+
+const CONTRACT_ADDRESS = config.networks['5777'].address;
