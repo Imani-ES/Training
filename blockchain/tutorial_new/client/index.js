cdaad9c6cce8ded652c097e33c35a9e25d45c5c7
Imani-ES <imanimuh@buffalo.edu>
2022-07-10 22:52:57 -0400

    web 3 connected
    successfully displayed connected web3 account on localhost

--- blockchain/tutorial_new/client/index.js
+++ blockchain/tutorial_new/client/index.js
@@ -3,3 +3,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 import config from '../build/contracts/Tickets.json'
 
 const CONTRACT_ADDRESS = config.networks['5777'].address;
+const contract_abi = config.abi;
+
+const web3 = new Web3(Web3.givenProvider || 'localhost:7545');
+const contract = new web3.eth.Contract(contract_abi,CONTRACT_ADDRESS);
+
+let account;
+
+const account_El = document.getElementById('account')
+const main = async () => {
+    const accounts = await web3.eth.requestAccounts();
+    account = accounts[0];
+    account_El.innerText = account
+};
+
+main();
